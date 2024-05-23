f66f04cc8a9393fd3dbf70692052b52da835ccbd
testgithubtiwari <rt936649@gmail.com>
2024-05-23 19:14:15 +0530

    dockerfile added and ci cd done

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 export let dbInstance = undefined;
 const connectDb = async () => {
   try {
-    const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.mxhckjt.mongodb.net/asset-monitoring?retryWrites=true&w=majority&appName=Cluster0`;
+    const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.zba4uok.mongodb.net/`;
     const connectionInstance = await mongoose.connect(connectionString);
-
     dbInstance = connectionInstance;
     console.log(
       `\n☘️  MongoDB Connected! Db host: ${connectionInstance.connection.host}\n`
