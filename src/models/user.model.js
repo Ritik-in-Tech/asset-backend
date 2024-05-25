57dea34ff3c3042d3bfb8612f6c9f6073f6804a8
testgithubtiwari <rt936649@gmail.com>
2024-05-25 10:59:03 +0530

    authentication controllers and models completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,2 +1,47 @@
 import { Schema, model } from "mongoose";
+import {
+  contactNumberSchema,
+  commonStringConstraints,
+} from "../utils/helpers/schema.helper.js";
 
+const businessSchema = new Schema(
+  {
+    name: commonStringConstraints,
+    userType: {
+      type: String,
+      enum: ["Insider", "Outsider"],
+      required: true,
+    },
+    businessId: {
+      type: Schema.Types.ObjectId,
+    },
+  },
+  {
+    _id: false,
+  }
+);
+const userSchema = new Schema({
+  name: commonStringConstraints,
+  jobTitle: commonStringConstraints,
+  avatar: {
+    type: String,
+  },
+  contactNumber: contactNumberSchema,
+  business: [businessSchema],
+  notificationViewCounter: {
+    default: 0,
+    type: Number,
+  },
+  email: {
+    type: String,
+    trim: true,
+    default: "",
+    match: [/\S+@\S+\.\S+/, "Please provide a valid email address"],
+  },
+  fcmToken: {
+    type: String,
+  },
+});
+
+const User = model("Users", userSchema);
+export { User };
