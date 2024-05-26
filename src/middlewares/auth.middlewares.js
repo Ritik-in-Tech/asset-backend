74eabdc9b911f252db165d20f676dbc063c995cc
testgithubtiwari <rt936649@gmail.com>
2024-05-26 11:01:03 +0530

    middle ware function little bit changed

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -3,7 +3,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 const verifyJWT = asyncHandler(async (req, res, next) => {
+  // console.log(process.env.ACCESS_TOKEN_SECRET);
   try {
     const token =
       req.cookies?.accessToken ||
@@ -14,6 +17,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
         .json(new ApiResponse(401, {}, "Unauthorized request"));
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    // console.log(decodedToken);
     if (
       !decodedToken ||
       !decodedToken?.userDetails ||
@@ -27,6 +31,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     }
 
     req.user = decodedToken?.userDetails;
+    // console.log(decodedToken?.userDetails.name);
     req.userDetails = decodedToken?.userDetails;
     next();
   } catch (error) {
