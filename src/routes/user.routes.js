db5ba7704650c4c0561f068e2663d8d920226b94
testgithubtiwari <rt936649@gmail.com>
2024-05-26 07:20:31 +0530

    users basic api created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,30 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { getUser } from "../controllers/user/getuser.contoller.js";
+import { getUserNotification } from "../controllers/user/getusernotification.controller.js";
+import { updateUserName } from "../controllers/user/updateusername.controller.js";
+import { setFCMToken } from "../controllers/user/setfcmtoken.controller.js";
+import { updateUserAvtar } from "../controllers/user/updateuseravatar.controller.js";
+import { getUserAvatar } from "../controllers/user/getuseravatart.controller.js";
 const router = Router();
-
+// the user route always have to verify the jwt token using the auth token provided in authorization
 router.use(verifyJWT);
 
+// router to get the user deatils
 router.route("/get-user").get(getUser);
 
+// router to get the notifications for the specific user
+router.route("/get-notification-user/:businessId").get(getUserNotification);
+
+// router to update the user name
+router.route("/update-user-name/:newName").patch(updateUserName);
+
+// router to update the FCM token
+router.route("/set-fcm-token").patch(setFCMToken);
+
+// router to update the user avatar
+router.route("/update-user-avatar").patch(updateUserAvtar);
+
+// router to get user avatar
+router.route("/get-user-avatar").get(getUserAvatar);
 export default router;
