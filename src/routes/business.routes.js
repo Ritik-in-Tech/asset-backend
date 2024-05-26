643222bf349c731d8d5f473ae02cc1e1dbac866a
testgithubtiwari <rt936649@gmail.com>
2024-05-26 17:40:44 +0530

    business routes changes done

--- src/routes/business.routes.js
+++ src/routes/business.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { createBusiness } from "../controllers/business/createbusiness.controller.js";
 import { updateBusinessDetails } from "../controllers/business/updatebusiness.controller.js";
+import { joinBusiness } from "../controllers/business/joinbusiness.controller.js";
 const router = Router();
 
 router.use(verifyJWT);
@@ -13,4 +14,7 @@ router.route("/create-business").post(createBusiness);
 router
   .route("/update-business-deatils/:businessId")
   .patch(updateBusinessDetails);
+
+// join business
+router.route("/join-business/:businessCode").post(joinBusiness);
 export default router;
