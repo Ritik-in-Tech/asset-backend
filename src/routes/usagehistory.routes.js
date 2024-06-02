023bfcaae0fd7cad1eb598722539207c46296f1a
testgithubtiwari <rt936649@gmail.com>
2024-06-02 16:41:54 +0530

    new routes added in usage history

--- src/routes/usagehistory.routes.js
+++ src/routes/usagehistory.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { addUsageHistory } from "../controllers/usagehistory/addusagehistory.controller.js";
+import {
+  addUsageHistory,
+  getConsumptionDataSpecificAsset,
+  getUsageHistory,
+} from "../controllers/usagehistory/addusagehistory.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -7,5 +11,11 @@ const router = Router();
 // router.use(verifyJWT);
 // router to add the usage History
 router.route("/add-usage-history/:assetId").post(addUsageHistory);
+
+router.route("/get-usage-history/:assetId").get(getUsageHistory);
+
+router
+  .route("/get-consumption-specific-asset/:assetId")
+  .get(getConsumptionDataSpecificAsset);
 
 export default router;
