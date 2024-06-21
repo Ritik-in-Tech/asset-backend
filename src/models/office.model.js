bc739ebee1414c6663986a272d03a26b712e7b75
testgithubtiwari <rt936649@gmail.com>
2024-06-21 21:01:51 +0530

    create office hierarchy done

--- src/models/office.model.js
+++ src/models/office.model.js
@@ -1,14 +1,33 @@
 import { Schema, model } from "mongoose";
 import { commonStringConstraints } from "../utils/helpers/schema.helper.js";
 
+const assetSchema = new Schema(
+  {
+    assetName: commonStringConstraints,
+    assetId: {
+      type: Schema.Types.ObjectId,
+    },
+  },
+  {
+    _id: false,
+  }
+);
+
 const officeSchema = new Schema({
   businessId: {
     type: Schema.Types.ObjectId,
   },
+  parnetOfficeId: {
+    type: Schema.Types.ObjectId,
+  },
+  subordinates: {
+    type: [Schema.Types.ObjectId],
+  },
+  allSubordinates: {
+    type: [Schema.Types.ObjectId],
+  },
   officeName: commonStringConstraints,
-  officeCity: commonStringConstraints,
-  officeState: commonStringConstraints,
-  officeDirection: commonStringConstraints,
+  assets: [assetSchema],
 });
 
 const Office = model("Office", officeSchema);
